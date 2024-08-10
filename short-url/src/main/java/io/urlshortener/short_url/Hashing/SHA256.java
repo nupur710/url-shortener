81dc4f603c65321db32bfbb22fe39e280a0c44fd
Nupur Singh <nupur.singh2410@gmail.com>
2024-08-10 23:40:41 +0530

    fix message scheduling implementation, use built-in integer rotation

--- short-url/src/main/java/io/urlshortener/short_url/Hashing/SHA256.java
+++ short-url/src/main/java/io/urlshortener/short_url/Hashing/SHA256.java
@@ -3,9 +3,9 @@ package io.urlshortener.short_url.Hashing;
 import java.util.ArrayList;
 
 public class SHA256 {
-    static int[] hash= {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+    static final int[] INITIAL_HASH_VALUES = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
             0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
-    static int[] k= {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    static final int[] K = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
             0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
             0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
             0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -26,13 +26,7 @@ public class SHA256 {
         //convert string to binary
         byte[] b= input.getBytes();
         //calculate len of byteArr
-        int x= 0, len= 0;
-        while(x < 512) {
-            if(((b.length*8) +1 + x + 64) % 512 == 0) {
-                break;
-            } x++;
-        }
-        len= ((b.length*8)+1+x+64)/8;
+        int len= ((b.length*8) +1 + 64 + 511) / 512 * 64;
         ArrayList<Byte> byteArr= new ArrayList<Byte>(len);
         for(int i= 0; i< b.length; i++) {
             byteArr.add(b[i]);
@@ -44,20 +38,15 @@ public class SHA256 {
             byteArr.add((byte) 0x00);
         }
         //append 64 bits of big endian integer
-        byte[] end= new byte[8];
-        for(int i= 0; i < 8; i++) {
-            end[7 - i]= (byte) ((input.length()*8) >>> (i*8)); //to append input len as a big endian integer, unsigned right shift operation
-        }
-        for(byte bt : end) {
-            byteArr.add(bt);
-        }
-        if((((byteArr.size() * 8) % 512) != 0) && (byteArr.size() != len)) {
-            throw new AssertionError("Final byte array length should be a multiple of 512 but is " + byteArr.size());
+        long bitLen = (long) b.length * 8;
+        for (int i= 7; i>= 0; i--) {
+            byteArr.add((byte) (bitLen >>> (i*8)));
         }
         return byteArr;
     }
 
     public static String chunkLoops(String url) {
+        int[] hash = INITIAL_HASH_VALUES.clone();
         ArrayList<Byte> input = preprocessing(url);
         /**
          * For every 512 bit chunk of input, create a message schedule
@@ -69,18 +58,28 @@ public class SHA256 {
             //for inner loop start i at chunk & go up till chunk + 64;update chunk to chunk+64
             int[] messageSchedule = new int[64];
             int j = 0;
-            for (int i = chunk; i < chunk+64; i += 4) {
-                messageSchedule[j] = (input.get(i) << 24) | (input.get(i + 1) << 16) | (input.get(i + 2) << 8) | input.get(i + 3);
+            /**
+             * When bitwise manipulation is performed on byte, it is promoted to int & undergoes
+             * sign extension. So, if byte value is negative (MSB-1) when it is converted to int,
+             * all higher order bits in the int will be set to 1. We need to perform 0xFF operation
+             * to preserve the unsigned 8-bit value of byte. This masks off higher order bits & retains
+             * only the lower 8-bits. Basically, the converting byte as an unsigned value b/w 0-255
+             */
+            for (int i = chunk; i < chunk + 64; i += 4) {
+                messageSchedule[j] = ((input.get(i) & 0xFF) << 24) | ((input.get(i + 1) & 0xFF) << 16)
+                        | ((input.get(i + 2) & 0xFF) << 8) | (input.get(i + 3) & 0xFF);
                 j++;
             }
 
-            for(int i= 16; i< messageSchedule.length; i++) {
-                int s0= (rightRotate(messageSchedule[i-15], 7)) ^ (rightRotate(messageSchedule[i-15], 18)) ^ (messageSchedule[i-15]>>>3);
-                int s1= (rightRotate(messageSchedule[i-2], 7)) ^ (rightRotate(messageSchedule[i-2], 19)) ^ (messageSchedule[i-2]>>>10);
-                messageSchedule[i]= messageSchedule[i-16] + s0 + messageSchedule[i-7] + s1;
+            for (int i = 16; i< 64; i++) {
+                int s0 = Integer.rotateRight(messageSchedule[i - 15], 7) ^ Integer.rotateRight(messageSchedule[i - 15], 18)
+                        ^ (messageSchedule[i - 15] >>> 3);
+                int s1 = Integer.rotateRight(messageSchedule[i - 2], 17) ^ Integer.rotateRight(messageSchedule[i - 2], 19)
+                        ^ (messageSchedule[i - 2] >>> 10);
+                messageSchedule[i] = messageSchedule[i - 16] + s0 + messageSchedule[i - 7] + s1;
             }
-            compress(messageSchedule);
-            chunk= chunk+64;
+            compress(hash, messageSchedule);
+            chunk+= 64;
         }
 
         StringBuilder str= new StringBuilder();
@@ -90,14 +89,14 @@ public class SHA256 {
         return str.toString();
     }
 
-    public static void compress(int[] messageSchedule) {
+    public static void compress(int[] hash, int[] messageSchedule) {
         int a= hash[0], b= hash[1], c= hash[2], d= hash[3], e= hash[4],
         f= hash[5], g= hash[6], h= hash[7];
         for(int i= 0; i < 64; i++) {
-            int s1= (rightRotate(e, 6)) ^ (rightRotate(e, 11)) ^ (rightRotate(e, 25));
+            int s1= Integer.rotateRight(e, 6) ^ Integer.rotateRight(e, 11) ^ Integer.rotateRight(e, 25);
             int ch= (e&f)^((~e)&g);
-            int temp1= h + s1 + ch + k[i] +messageSchedule[i];
-            int s0= (rightRotate(a, 2)) ^ (rightRotate(a, 13)) ^ (rightRotate(a, 22));
+            int temp1= h + s1 + ch + K[i] +messageSchedule[i];
+            int s0= Integer.rotateRight(a, 2) ^ Integer.rotateRight(a, 13) ^ Integer.rotateRight(a, 22);
             int maj= (a&b)^(a&c)^(b&c);
             int temp2= s0 + maj;
             h= g;
@@ -117,11 +116,5 @@ public class SHA256 {
         hash[5]+= f;
         hash[6]+= g;
         hash[7]+= h;
-    }
-
-    public static int rightRotate(int n, int d) {
-        int bits= 32;
-        d= d % 32; //for when d > 32
-        return (n >>> d) | (n << (bits-d));
     }
 }
